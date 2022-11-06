e7abf931c078004199770b4fca9f4855ca5a5e7b
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-11-06 17:22:54 +0400

    Курс 2.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         String[] in = {"каждый", "охотник", "каждый", "знать", "знать"};
         for (int i = 0; i < in.length; i++) {
             System.out.print(in[i] + " ");
@@ -15,5 +16,42 @@ public class Main {
             for (int i = 0; i < out.length; i++) {
                 System.out.print(out[i] + " ");
             }
+        */
+        // Задача 1.
+
+        // объявление двумерного массива строк
+        String[][] BookAndAuthor = new String[2][2];
+
+        BookAndAuthor[0][0] = "Название книги: 'Капитанская дочка'";
+        BookAndAuthor[1][0] = "Название книги: 'Война и мир'";
+
+        BookAndAuthor[0][1] = "Автор - Александр Сергеевич";
+        BookAndAuthor[1][1] = "Автор - Лев Николаевич";
+
+        // вывод результата
+        for (int i=0; i<BookAndAuthor.length; i++) {
+            for (int j=0; j<BookAndAuthor[i].length; j++)
+                System.out.print(BookAndAuthor[i][j] + " ");
+            System.out.println();
         }
+
+        // Задача 2.
+        /*
+        catch (IOException ex) {logger.error(ex);
+            throw new MyException(ex.getMessage());
+            catch (SQLException ex) {
+                logger.error(ex);
+                throw new MyException(ex.getMessage());
+            }catch (Exception ex) {
+                logger.error(ex);
+                throw new MyException(ex.getMessage());
+            }
+
+        catch(IOException | SQLException | Exception ex){
+            logger.error(ex);
+            throw new MyException(ex.getMessage());
+        }
+
+        */
     }
+}
